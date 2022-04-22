5c9627a91940193c885f5898e84d294e1d2667b9
SeungJin Kim <64381@hufs.ac.kr>
2022-04-22 13:09:40 +0900

    서버용 파일 (node.js 사용)

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+var fs = require('fs');
+const app = express();
+
+app.listen(8080, function(){
+  console.log('listening on 8080');
+});
+
+app.get('/Park', function(req, res){
+  res.send("이미 죽은 군인이다");
+});
+
+app.get('/Gang', function(req, res){
+  res.send("오늘 죽은 군인이다");
+});
+
+app.get('/GangJ', function(req, res){
+  res.send("한달 뒤 죽을 군인이다");
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/imgZ',function(req, res){
+  fs.readFile('img/RIP_Z.png',function(error, data){
+    res.writeHead(200,{'Content-type': 'text/html'});
+    res.end(data);
+  });
+});
+
+app.get('/imgJ',function(req, res){
+  fs.readFile('img/RIP_J.png',function(error, data){
+    res.writeHead(200,{'Content-type': 'text/html'});
+    res.end(data);
+  });
+});
